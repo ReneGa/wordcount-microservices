6f4395e924f04d9e85e668ccf933a6815665e6fc
Sergey Grebenshchikov <sgreben@gmail.com>
2017-06-26 16:12:24 +0200

    Add Copy method to WordCount

--- windower/domain/tweetWordCount.go
+++ windower/domain/tweetWordCount.go
@@ -1,8 +1,18 @@
 package domain
 
-import "time"
+import (
+	"time"
+)
 
 type WordCount map[string]int
+
+func (w WordCount) Copy() WordCount {
+	copy := WordCount{}
+	for word, count := range w {
+		copy[word] = count
+	}
+	return copy
+}
 
 type TweetWordCount struct {
 	WordCount     WordCount
