cbec2d2c58fc4dd912528da6c6b494f291199572
Sergey Grebenshchikov <sgreben@gmail.com>
2017-07-01 13:10:58 +0200

    Refactor ingestor gateway

--- ingestor/gateway/twitter.go
+++ ingestor/gateway/twitter.go
@@ -4,10 +4,9 @@ import (
 	"net/url"
 	"time"
 
-	"reflect"
-
 	"github.com/ReneGa/tweetcount-microservices/ingestor/client"
 	"github.com/ReneGa/tweetcount-microservices/ingestor/domain"
+	"github.com/chimeracoder/anaconda"
 )
 
 // Twitter is a gateway to the Twitter streaming API
@@ -26,13 +25,51 @@ type twitter struct {
 	newTwitterAPI func() client.TwitterAPI
 }
 
+func convertTweet(anacondaTweet anaconda.Tweet) domain.Tweet {
+	Text := anacondaTweet.Text
+	ID := anacondaTweet.IdStr
+	Language := anacondaTweet.Lang
+	timeString := anacondaTweet.CreatedAt
+	Time, _ := time.Parse(time.RubyDate, timeString)
+	return domain.Tweet{
+		Text:     Text,
+		ID:       ID,
+		Time:     Time,
+		Language: Language,
+	}
+}
+
+func processStream(stream client.TwitterStream, out chan domain.Tweet, stop chan bool) {
+	items := stream.C()
+	defer close(out)
+	defer stream.Stop()
+	for {
+		select {
+		case item := <-items:
+			if item != nil {
+				switch typedItem := item.(type) {
+				case anaconda.Tweet:
+					tweet := convertTweet(typedItem)
+					select {
+					case out <- tweet:
+					case <-stop:
+						return
+					}
+				default:
+				}
+			}
+		case <-stop:
+			return
+		}
+	}
+}
+
 // Tweets returns a stream of public Tweets for a given search query.
 func (a twitter) Tweets(query string) domain.Tweets {
 	api := a.newTwitterAPI()
 	stream := api.PublicStreamFilter(url.Values{
 		"track": {query},
 	})
-	items := stream.C()
 	out := make(chan domain.Tweet)
 	stop := make(chan bool)
 
@@ -41,35 +78,7 @@ func (a twitter) Tweets(query string) domain.Tweets {
 		Stop: stop,
 	}
 
-	go func() {
-		defer close(out)
-		defer stream.Stop()
-		for {
-			select {
-			case item := <-items:
-				if item != nil {
-					itemValue := reflect.ValueOf(item)
-					Text := itemValue.FieldByName("Text").String()
-					ID := itemValue.FieldByName("IdStr").String()
-					Language := itemValue.FieldByName("Lang").String()
-					timeString := itemValue.FieldByName("CreatedAt").String()
-					Time, _ := time.Parse(time.RubyDate, timeString)
-					select {
-					case out <- domain.Tweet{
-						Text:     Text,
-						ID:       ID,
-						Time:     Time,
-						Language: Language,
-					}:
-					case <-stop:
-						return
-					}
-				}
-			case <-stop:
-				return
-			}
-		}
-	}()
+	go processStream(stream, out, stop)
 
 	return tweets
 }
